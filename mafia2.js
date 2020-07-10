850d68ded07fde69ee780fb38483f66816361cd6
margad <margd@multiple-ppls-iMac.local>
2020-07-10 14:52:57 +0800

    margd

--- mafia2.js
+++ mafia2.js
@@ -1,22 +1,34 @@
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAMppKcZo4sa9GjtknjiRyCVt2_yNexh9M",
-    authDomain: "team-up-aff0e.firebaseapp.com",
-    databaseURL: "https://team-up-aff0e.firebaseio.com",
-    projectId: "team-up-aff0e",
-    storageBucket: "team-up-aff0e.appspot.com",
-    messagingSenderId: "874195963351",
-    appId: "1:874195963351:web:d3a1d27d2f4225dff54f92",
-    measurementId: "G-WRG3X3HCKY"
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC78FRamszBxuCmSeL8ZGhduuXeqqrBnf4",
+    authDomain: "team-up-73173.firebaseapp.com",
+    databaseURL: "https://team-up-73173.firebaseio.com",
+    projectId: "team-up-73173",
+    storageBucket: "team-up-73173.appspot.com",
+    messagingSenderId: "1030300585767",
+    appId: "1:1030300585767:web:6577af963515d152b32302"
   };
-  firebase.initializeApp(firebaseConfig);
-  const db=firebase.firestore();
+firebase.initializeApp(firebaseConfig);
+const db=firebase.firestore();
+
 const ready = () => {
   document.getElementById("ready").classList.toggle('green');
 }
 
 const Send = () => {
   const Input = document.getElementById('Input');
-  const inputValue = document.getElementById("Input").nodeValue;
-    
+  const inputValue = document.getElementById("Input").value;
+  const t = document.createTextNode(inputValue);
+  document.getElementsByClassName('chatbox')[0].appendChild(t);
+  document.getElementById('Input').value = " "
+  db.collection('rooms/tt/Chat').add ({
+    text: inputValue
+  })
 }
+db.collection("rooms/tt/Chat")[0].onSnapshot(function(doc) {
+        console.log(doc.data())
+    });
+
+
+

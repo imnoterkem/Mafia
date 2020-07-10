a5b426a96272f8c0aae9c5144a41f130fe9dc3c0
imnoterkem <e.erkhem@yahoo.com>
2020-07-10 14:55:01 +0800

    dutu

--- mafia.js
+++ mafia.js
@@ -1,24 +1,25 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAMppKcZo4sa9GjtknjiRyCVt2_yNexh9M",
-    authDomain: "team-up-aff0e.firebaseapp.com",
-    databaseURL: "https://team-up-aff0e.firebaseio.com",
-    projectId: "team-up-aff0e",
-    storageBucket: "team-up-aff0e.appspot.com",
-    messagingSenderId: "874195963351",
-    appId: "1:874195963351:web:d3a1d27d2f4225dff54f92",
-    measurementId: "G-WRG3X3HCKY"
+    apiKey: "AIzaSyC78FRamszBxuCmSeL8ZGhduuXeqqrBnf4",
+    authDomain: "team-up-73173.firebaseapp.com",
+    databaseURL: "https://team-up-73173.firebaseio.com",
+    projectId: "team-up-73173",
+    storageBucket: "team-up-73173.appspot.com",
+    messagingSenderId: "1030300585767",
+    appId: "1:1030300585767:web:6577af963515d152b32302",
+    appId: "1:1030300585767:web:6577af963515d152b32302"
 };
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 const db = firebase.database();
+
 function Join() {
     let s = document.getElementById("name").value
     if (s !== '') {
         db.ref(`Room/${s}/players`).once("value").then(snap => {
 
-            db.ref(`Room/${s}`).update({ players: parseInt(snap.val())+1})
+            db.ref(`Room/${s}`).update({ players: parseInt(snap.val()) + 1 })
             window.location.href = `mafia2.html?/${s}`
-            
+
 
         })
     }
@@ -42,18 +43,3 @@ function Host() {
         }
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ mongolia.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    while(0<1){
+        cout<<"mongolia";
+    }
+}

815170ce626b462f77b3b4d995e442d8011b6537
imnoterkem <e.erkhem@yahoo.com>
2020-07-22 15:15:23 +0800

    margad

--- mafia2.js
+++ mafia2.js
@@ -10,32 +10,48 @@
   };
   firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
+  let useruid = null;
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+          // User is signed in.
+          var isAnonymous = user.isAnonymous;
+          useruid = user.uid;
+          db.doc(`users/${useruid}`).set({
+              name: "tester1"
+          })
+          console.log(useruid);
+      } else {
+          // User is signed out.
+          // ...
+      }
+      // ...
+  });
 
   const ready = () => {
       document.getElementById("ready").classList.toggle('green');
   }
   let roomname = new URL(window.location.href).searchParams.get("r");
   const Send = () => {
-    const Input = document.getElementById('Input');
-    if (Input.value === '' ){
-      return;
-    }
-    let s=0;
-    while(Input.value[s]===" "){
-      s++;
-    }
-    Input.value=Input.value.slice(s, Input.value.length)
-    if (Input.value === '' ){
-      return;
-    }
-      const d = document.createElement('div');
-      d.innerHTML = Input.value.trim();
-      d.classList.add('msgs')
-      document.getElementsByClassName('chatbox')[0].appendChild(d);
-      
+      const Input = document.getElementById('Input');
+      if (Input.value === '') {
+          return;
+      }
+      let s = 0;
+      while (Input.value[s] === " ") {
+          s++;
+      }
+      Input.value = Input.value.slice(s, Input.value.length)
+      if (Input.value === '') {
+          return;
+      }
+      //   const d = document.createElement('div');
+      //   d.innerHTML = Input.value.trim();
+      //   d.classList.add('msgs')
+      //   document.getElementsByClassName('chatbox')[0].appendChild(d);
       db.collection(`rooms/${roomname}/Chat`).add({
           createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-          text: Input.value
+          text: Input.value,
+          sender: useruid && useruid
       })
       document.getElementById('Input').value = "";
       document.getElementsByClassName('chatbox')[0].scrollTop = document.getElementsByClassName('chatbox')[0].scrollHeight;
@@ -45,27 +61,31 @@
 
   db.collection(`rooms`).doc(`${roomname}`).collection('Chat').orderBy('createdAt')
       .onSnapshot(function(querySnapshot) {
-
-          // console.log(docs);
-          // for (var i = 0; i < docs.size; i++) {
-          //   console.log(docs[i].data().text);
-          // }
           document.getElementsByClassName('chatbox')[0].innerHTML = ''
           querySnapshot.forEach(function(doc) {
               // cities.push(doc.data().name);
               // console.log(doc.data())
-
+              let nickname;
               const t = document.createElement("div")
-              t.innerHTML = doc.data().text;
-              t.classList.add('msgs');
-              document.getElementsByClassName('chatbox')[0].append(t);
+              db.collection("users").doc(`${doc.data().sender}`).onSnapshot(function(docu) {
+                  if (docu.exists) {
+
+                      nickname = docu.data().name;
+                      t.innerHTML = nickname + ':' + doc.data().text;
+                      t.classList.add('msgs');
+                      document.getElementsByClassName('chatbox')[0].append(t);
+                      document.getElementsByClassName('chatbox')[0].scrollTop = document.getElementsByClassName('chatbox')[0].scrollHeight;
+                  } else {
+                      //onsole.log("gg");
+                  }
+              });
+
 
           });
-          document.getElementsByClassName('chatbox')[0].scrollTop = document.getElementsByClassName('chatbox')[0].scrollHeight;
+
       });
   let input = document.getElementById("Input");
   document.onkeyup = (event) => {
-      console.log("1111");
       if (event.keyCode === 13) {
           Send();
       }

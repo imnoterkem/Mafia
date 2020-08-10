a5cc148a94c8f1cc76bfd510268a54f02d7ba8fe
margad <margd@multiple-ppls-iMac.local>
2020-08-10 15:30:47 +0800

    pull

--- public/mafia3.js
+++ public/mafia3.js
@@ -10,7 +10,7 @@ var firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore(app);
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         var isAnonymous = user.isAnonymous;
         useruid = user.uid;
@@ -19,10 +19,10 @@ firebase.auth().onAuthStateChanged(function(user) {
     }
 });
 let roomname = new URL(window.location.href).searchParams.get("r");
-db.collection(`rooms/${roomname}/users`).get().then(function(doc) {
+db.collection(`rooms/${roomname}/users`).get().then(function (doc) {
     let i = 0;
     let color = ['#5781EC', '#FFB4B4', '#ECDE5C', '#FFB03A', '#0AA119', '#A812EE', '#FFFFFF']
-    doc.forEach(function(docu) {
+    doc.forEach(function (docu) {
         document.getElementsByClassName("player-name")[i].style.color = color[i]
         document.getElementsByClassName("player-name")[i].innerHTML = docu.data().name
         db.doc(`rooms/${roomname}/users/${docu.id}`).update({
@@ -32,15 +32,15 @@ db.collection(`rooms/${roomname}/users`).get().then(function(doc) {
 
     })
 })
-db.doc(`rooms/${roomname}`).get().then(function(doc) {
+db.doc(`rooms/${roomname}`).get().then(function (doc) {
     if (!doc.data().isChatDeleted) {
-        db.doc(`rooms/${ roomname }`).update({
+        db.doc(`rooms/${roomname}`).update({
             isChatDeleted: true
         })
-        db.collection(`rooms/${ roomname }/Chat`).get().then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
+        db.collection(`rooms/${roomname}/Chat`).get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
 
-                db.collection(`rooms/${roomname}/Chat`).doc(`${doc.id}`).delete().then(function() {}).catch(function(error) {
+                db.collection(`rooms/${roomname}/Chat`).doc(`${doc.id}`).delete().then(function () { }).catch(function (error) {
                     console.error("Error removing document: ", error);
                 });
             });
@@ -66,7 +66,7 @@ db.doc(`rooms/${roomname}`).update({
 let players = [];
 
 
-db.doc(`rooms/${roomname}`).get().then(function(doc) {
+db.doc(`rooms/${roomname}`).get().then(function (doc) {
     console.log(doc.data().shuffled)
     if (!doc.data().shuffled) {
 
@@ -75,8 +75,8 @@ db.doc(`rooms/${roomname}`).get().then(function(doc) {
         })
         let arr = []
 
-        db.collection(`rooms/${roomname}/users`).get().then(function(querySnapshot) {
-            querySnapshot.forEach(function(docu) {
+        db.collection(`rooms/${roomname}/users`).get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (docu) {
                 arr.push(docu.id);
                 console.log(docu.id);
 
@@ -115,78 +115,64 @@ db.doc(`rooms/${roomname}`).get().then(function(doc) {
 })
 
 const ready = () => {
-        document.getElementsByClassName("ready")[0].classList.toggle('green');
-        if (clicked % 2 === 0) {
-            db.doc(`rooms/${roomname}`).get().then(function(doc) {
-                let readynumber = doc.data().ready + 1;
-                db.doc(`rooms/${roomname}`).update({
-                    ready: readynumber
-                })
+    document.getElementsByClassName("ready")[0].classList.toggle('green');
+    if (clicked % 2 === 0) {
+        db.doc(`rooms/${roomname}`).get().then(function (doc) {
+            let readynumber = doc.data().ready + 1;
+            db.doc(`rooms/${roomname}`).update({
+                ready: readynumber
             })
-            db.doc(`rooms/${roomname}/users/${useruid}`).update({
-                ready: true
+        })
+        db.doc(`rooms/${roomname}/users/${useruid}`).update({
+            ready: true
+        })
+    } else {
+        db.doc(`rooms/${roomname}/users/${useruid}`).update({
+            ready: false
+        })
+        db.doc(`rooms/${roomname}`).get().then(function (doc) {
+            let readynumber = doc.data().ready - 1;
+            db.doc(`rooms/${roomname}`).update({
+                ready: readynumber
             })
-        } else {
-            db.doc(`rooms/${roomname}/users/${useruid}`).update({
-                ready: false
-            })
-            db.doc(`rooms/${roomname}`).get().then(function(doc) {
-                let readynumber = doc.data().ready - 1;
-                db.doc(`rooms/${roomname}`).update({
-                    ready: readynumber
-                })
-            })
-        }
-
-        clicked = clicked + 1;
+        })
     }
-<<<<<<< HEAD
-})
-db.doc(`rooms/${roomname}`).get().then(function (doc) {
-    console.log("idadadaa")
-    const Send = () => {
-        let Input = document.getElementById('Input');
 
-        if (Input.value === '') {
-            return;
-        }
-        let s = 0;
-        while (Input.value[s] === " ") {
-            s++;
-=======
-    // udur bolgodiin
+    clicked = clicked + 1;
+}
+// udur bolgodiin
 let shunu = document.createElement('div').innerHTML = 'шөнө 111'
-db.doc(`rooms/${roomname}`).onSnapshot(function(doc) {
-        console.log(doc.data());
-        if (doc.data().ready == 7) {
-            if (doc.data().time == 'day') {
-                console.log('nice');
-                document.getElementsByClassName('h')[0].style.background = "linear-gradient(to bottom, #001447, #000000)";
-                document.getElementsByClassName('body')[0].backgroundImage = "url('assets/nighttown.png')";
-                db.doc(`rooms/${roomname}`).update({
-                    time: 'night'
-                });
-                db.doc(`rooms/${roomname}`).update({
-                    ready: 0
-                })
-                document.getElementsByClassName('ready')[0].background = "#3AC348"
-            }
-            if (doc.data().time == 'night') {
-                document.getElementsByClassName('h')[0].style.background = "linear-gradient(to bottom, #62b8e8, #FFFFFF)";
-                document.getElementsByClassName('body')[0].backgroundImage = "url('assets/daytown.png')";
-                document.getElementsByClassName('night').appendChild(shunu)
-                db.doc(`rooms/${roomname}`).update({
-                    time: 'day'
-                });
-                db.doc(`rooms/${roomname}`).update({
-                    ready: 0
-                })
-                document.getElementsByClassName('ready')[0].background = "#3AC348"
-            }
+db.doc(`rooms/${roomname}`).onSnapshot(function (doc) {
+    console.log(doc.data());
+    if (doc.data().ready == 7) {
+        if (doc.data().time == 'day') {
+            console.log('nice');
+            document.getElementsByClassName('h')[0].style.background = "linear-gradient(to bottom, #001447, #000000)";
+            document.getElementsByClassName('body')[0].backgroundImage = "url('assets/nighttown.png')";
+            db.doc(`rooms/${roomname}`).update({
+                time: 'night'
+            });
+            db.doc(`rooms/${roomname}`).update({
+                ready: 0
+            })
+            document.getElementsByClassName('ready')[0].background = "#3AC348"
         }
-    })
-    //send
-db.doc(`rooms/${roomname}`).get().then(function(doc) {
+        if (doc.data().time == 'night') {
+            document.getElementsByClassName('h')[0].style.background = "linear-gradient(to bottom, #62b8e8, #FFFFFF)";
+            document.getElementsByClassName('body')[0].backgroundImage = "url('assets/daytown.png')";
+            document.getElementsByClassName('night').appendChild(shunu)
+            db.doc(`rooms/${roomname}`).update({
+                time: 'day'
+            });
+            db.doc(`rooms/${roomname}`).update({
+                ready: 0
+            })
+            document.getElementsByClassName('ready')[0].background = "#3AC348"
+        }
+    }
+})
+//send
+db.doc(`rooms/${roomname}`).get().then(function (doc) {
     if (doc.data().time == 'day') {
         const Send = () => {
             let Input = document.getElementById('Input');
@@ -200,61 +186,41 @@ db.doc(`rooms/${roomname}`).get().then(function(doc) {
             }
             Input.value = Input.value.slice(s, Input.value.length)
             let useless = document.createElement('div');
-            db.doc(`rooms/${roomname}/users/${useruid}`).get().then(function(doc) {
+            db.doc(`rooms/${roomname}/users/${useruid}`).get().then(function (doc) {
                 useless.innerHTML = doc.data().name + ' : ' + Input.value;
                 useless.classList.add('msgs');
                 document.getElementsByClassName('chatbox')[0].appendChild(useless);
             })
 
 
-            db.doc(`rooms/${roomname}/users/${useruid}`).get().then(function(doc) {
+            db.doc(`rooms/${roomname}/users/${useruid}`).get().then(function (doc) {
                 let sendername = doc.data().name;
                 console.log(Input.value)
                 db.collection(`rooms/${roomname}/Chat`).add({
                     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
                     text: Input.value,
                     sender: sendername
-                }).then(function() {
+                }).then(function () {
                     document.getElementById('Input').value = '';
                     document.getElementsByClassName('chatbox')[0].scrollTop = document.getElementsByClassName('chatbox')[0].scrollHeight;
                 })
             })
 
->>>>>>> 27132b7b21ed136a4ebec25d370ec4c86ff0acd5
         }
-        Input.value = Input.value.slice(s, Input.value.length)
-        let useless = document.createElement('div');
-        db.doc(`rooms/${roomname}/users/${useruid}`).get().then(function (doc) {
-            useless.innerHTML = doc.data().name + ' : ' + Input.value;
-            useless.classList.add('msgs');
-            document.getElementsByClassName('chatbox')[0].appendChild(useless);
-        })
-
-
-        db.doc(`rooms/${roomname}/users/${useruid}`).get().then(function (doc) {
-            let sendername = doc.data().name;
-            console.log(Input.value)
-            db.collection(`rooms/${roomname}/Chat`).add({
-                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-                text: Input.value,
-                sender: sendername
-            }).then(function () {
-                document.getElementById('Input').value = '';
-                document.getElementsByClassName('chatbox')[0].scrollTop = document.getElementsByClassName('chatbox')[0].scrollHeight;
-            })
-        })
-
     }
-
 })
+let day = true;
 let input = document.getElementById("Input");
 document.onkeyup = (event) => {
+
+
     if (event.keyCode === 13) {
-        Send();
+        console.log()
+        if (day) Send();
     }
 }
 
-db.doc(`rooms/${roomname}`).get().then(function(doc) {
+db.doc(`rooms/${roomname}`).get().then(function (doc) {
     if (!doc.data().shuffled) {
         db.doc(`rooms/${roomname}`).update({
             shuffled: true
@@ -294,7 +260,7 @@ const Send = () => {
     if (Input.value.trim() === '') return;
 
     Input.value = Input.value.trim();
-    db.doc(`rooms/${roomname}/users/${useruid}`).get().then(function(doc) {
+    db.doc(`rooms/${roomname}/users/${useruid}`).get().then(function (doc) {
 
         let sendername = doc.data().name;
         let color = doc.data().colors;
@@ -314,9 +280,9 @@ const Send = () => {
 console.log(roomname)
 
 db.collection(`rooms`).doc(`${roomname}`).collection('Chat').orderBy('createdAt')
-    .onSnapshot(function(querySnapshot) {
+    .onSnapshot(function (querySnapshot) {
         document.getElementsByClassName('display')[0].innerHTML = ''
-        querySnapshot.forEach(function(doc) {
+        querySnapshot.forEach(function (doc) {
             const t = document.createElement("div")
             t.innerHTML = doc.data().sender + ' : ' + doc.data().text;
             t.classList.add('msgs');
@@ -328,18 +294,19 @@ db.collection(`rooms`).doc(`${roomname}`).collection('Chat').orderBy('createdAt'
     });
 
 let timer = 10;
-let day = true;
+
 const mainTimer = () => {
     document.getElementById("timer").innerHTML = `Auto-Skipping in: ${timer}`;
     if (timer <= 0) {
         timer = 10;
-        db.doc(`rooms/${roomname}`).get().then(function(doc) {
+        db.doc(`rooms/${roomname}`).get().then(function (doc) {
             console.log(doc.data().time)
 
             if (doc.data().time == 'day') {
-                document.getElementById('send').disabled = false;
+
+                day = true;
                 console.log('nice');
-                document.getElementsByClassName('h')[0].style.backgroundImage ="url('assets/daytown.png')";
+                document.getElementsByClassName('h')[0].style.backgroundImage = "url('assets/nighttown.png')";
                 document.getElementsByClassName('body')[0].style.background = "linear-gradient(to bottom, #001447, #000000)";
                 document.getElementsByClassName('moon')[0].style.background = "#FFE99C";
                 db.doc(`rooms/${roomname}`).update({
@@ -350,10 +317,9 @@ const mainTimer = () => {
                 })
             }
             if (doc.data().time == "night") {
-                document.getElementById('send').disabled = true;
-                console.log(document.getElementById('send').disabled);
+                day = false;
                 console.log('sdfsdf')
-                document.getElementsByClassName('h')[0].style.backgroundImage =  "url('/assets/nighttown.png')";
+                document.getElementsByClassName('h')[0].style.backgroundImage = "url('assets/daytown.png')";
                 document.getElementsByClassName('body')[0].style.background = "linear-gradient(to bottom, #62b8e8, #FFFFFF)";
                 document.getElementsByClassName('moon')[0].style.background = "#F2D365";
                 db.doc(`rooms/${roomname}`).update({
